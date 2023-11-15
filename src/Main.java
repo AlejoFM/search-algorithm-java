cc7e2cfc8c80479e7d6f2d3d4845ac27161c4c25
Alejo Franzoni <alejofranzonimanassero@gmail.com>
2023-11-15 16:30:12 -0300

    Print to show positions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,16 +14,24 @@ public class Main {
 
 
         long startTimeJavaSearch = System.currentTimeMillis();
-        FindMethodByJava.findMethodJava(stringGenerated,patternGenerated);
+        List<Integer> searchResultJava = FindMethodByJava.findMethodJava(stringGenerated,patternGenerated);
         long stopTimeJavaSearch = System.currentTimeMillis();
         System.out.println("Elapsed time was " + (stopTimeJavaSearch - startTimeJavaSearch) + " miliseconds."); // Se muestra por consola el tiempo de ejecución.
-
+        showPositions(searchResultJava.listIterator(), patternGenerated.length());
 
 
         long startTimeAlejoSearch = System.currentTimeMillis();
-        FindMethodByAlejo.stringSearch(stringGenerated, patternGenerated);
+        List<Integer> searchResultAlejo = FindMethodByAlejo.stringSearch(stringGenerated, patternGenerated);
         long stopTimeAlejoSearch = System.currentTimeMillis();
         System.out.println("Elapsed time was " + (stopTimeAlejoSearch - startTimeAlejoSearch) + " miliseconds."); // Se muestra por consola el tiempo de ejecución.
-
+        showPositions(searchResultAlejo.listIterator(), patternGenerated.length());
+    }
+    private static void showPositions(ListIterator<Integer> iterator, int patternLength) {
+        System.out.println("Positions found:");
+        while (iterator.hasNext()) {
+            int startPosition = iterator.next();
+            int endPosition = startPosition + patternLength - 1;
+            System.out.println("Start position: " + startPosition + ", End position: " + endPosition);
+        }
     }
 }
